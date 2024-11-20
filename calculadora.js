d1f93d347417130faf5334c7a98dfc3937c5eff2
Kauã <kauasantosalmeida9@gmail.com>
2024-11-20 15:41:20 -0300

    calculadora

---
+++ calculadora.js
@@ -0,0 +1,36 @@
+let display = document.getElementById("display");
+let currentInput = "";
+let currentOperator = "";
+
+
+function appendNumber(value) {
+    currentInput += value
+    display.textContent = currentInput
+}
+
+
+function appendOperator(operator) {
+    if (currentInput === "" && operator !== ".") return
+    currentInput += operator;
+    display.textContent = currentInput;
+}
+
+
+function calculate() {
+    try {
+        let result = eval(currentInput)
+        if (!Number.isInteger(result)) {
+            result = result.toFixed(2)
+        }
+        currentInput = result;
+        display.textContent = currentInput;
+    } catch (error) {
+        display.textContent = "❌ Error ❌";
+        currentInput = "";
+    }
+}
+
+function clearDisplay(){
+    currentInput = "";
+    display.textContent = currentInput;
+}
